046884f55fa7e5bb7f0b1b422343fd5c8b716200
Evgenii Surikov <surikov96@mail.ru>
2024-03-18 13:34:17 +0700

    Getting an array via EL

--- src/main/java/org/surikov/homework7/HelloServlet.java
+++ src/main/java/org/surikov/homework7/HelloServlet.java
@@ -2,28 +2,21 @@ package org.surikov.homework7;
 
 import java.io.*;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
-    private String message;
 
-    public void init() {
-        message = "Hello World!";
-    }
-
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         response.setContentType("text/html");
-
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
+        String[] linkdata = new String[]{"RESULT JSP", "FORM"};
+        request.setAttribute("linkdata",linkdata);
+        String[] users = new String[] {"Tom", "Bob", "Sam"};
+        request.setAttribute("users", users);
+        getServletContext().getRequestDispatcher("/basic.jsp").forward(request, response);
 
     }
 
-    public void destroy() {
-    }
 }
